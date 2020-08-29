3773116efd299e14d2087848b386d15cd71dea8b
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-08-29 15:05:58 +0200

    Clean create_education(), reindent only

--- R/create_education.R
+++ R/create_education.R
@@ -1,10 +1,12 @@
-create_education <- function(course, institution, year) {
-
-c('',
-  '[[education.courses]]',
-  sprintf('  course = "%s"', course),
-  sprintf('  institution = "%s"', institution),
-  sprintf('  year = "%s"', year),
-  '')
-
+# create_education -------------------------------------------------------------
+create_education <- function(course, institution, year)
+{
+  c(
+    '',
+    '[[education.courses]]',
+    sprintf('  course = "%s"', course),
+    sprintf('  institution = "%s"', institution),
+    sprintf('  year = "%s"', year),
+    ''
+  )
 }
